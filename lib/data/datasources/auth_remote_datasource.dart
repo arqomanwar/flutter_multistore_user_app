5e6519c34e22d2aa2a1a3068b887b2bc832ed8c0
arqomanwar <arkomanwar@gmail.com>
2023-12-27 02:20:51 +0700

    04-product-detail-checkout

--- lib/data/datasources/auth_remote_datasource.dart
+++ lib/data/datasources/auth_remote_datasource.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:dartz/dartz.dart';
 import 'package:flutter_multistore_user_app/common/global_variables.dart';
+import 'package:flutter_multistore_user_app/data/datasources/auth_local_datasource.dart';
 import 'package:flutter_multistore_user_app/data/models/auth_response_model.dart';
 import 'package:flutter_multistore_user_app/data/models/request/login_request_model.dart';
 import 'package:flutter_multistore_user_app/data/models/request/register_request_model.dart';
@@ -24,7 +27,41 @@ class AuthRemoteDatasource {
     }
   }
 
-  logout() {}
+  Future<Either<String, AuthResponseModel>> login(
+      LoginRequestModel model) async {
+    final headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    };
+    final response = await http.post(
+        Uri.parse('${GlobalVariables.baseUrl}/api/login'),
+        headers: headers,
+        body: model.toJson());
 
-  login(LoginRequestModel model) {}
+    if (response.statusCode == 200) {
+      return Right(AuthResponseModel.fromJson(response.body));
+    } else {
+      final obj = jsonDecode(response.body);
+      return Left(obj['message']);
+    }
+  }
+
+  Future<Either<String, String>> logout() async {
+    final token = await AuthLocalDatasource().getToken();
+    final headers = {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $token',
+    };
+    final response = await http.post(
+      Uri.parse('${GlobalVariables.baseUrl}/api/logout'),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      return const Right('logout success');
+    } else {
+      return const Left('Server error');
+    }
+  }
 }
--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_multistore_user_app/data/datasources/auth_local_datasource.dart';
+import 'package:flutter_multistore_user_app/pages/auth/auth_page.dart';
+import 'package:flutter_multistore_user_app/pages/home/home_page.dart';
 
+import '../../bloc/logout/logout_bloc.dart';
 import '../../utils/images.dart';
 
 class DashboardPage extends StatefulWidget {
@@ -31,18 +35,50 @@ class _HomePageState extends State<DashboardPage> {
     });
 
     _screens = [
-      const Center(
-        child: Column(
-          children: [
-            Text('Home'),
-          ],
-        ),
-      ),
+      HomePage(),
       const Center(
         child: Text('Order'),
       ),
-      const Center(
-        child: Text('More'),
+      Center(
+        child: BlocConsumer<LogoutBloc, LogoutState>(
+          listener: (context, state) {
+            state.maybeWhen(
+              orElse: () {},
+              loaded: (message) {
+                AuthLocalDatasource().removeAuthData();
+                Navigator.pushAndRemoveUntil(context,
+                    MaterialPageRoute(builder: (context) {
+                  return const AuthPage();
+                }), (route) => false);
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Logout Successfully'),
+                  backgroundColor: Colors.blue,
+                ));
+              },
+              error: (message) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(message),
+                  backgroundColor: Colors.red,
+                ));
+              },
+            );
+          },
+          builder: (context, state) {
+            return state.maybeWhen(
+              orElse: () {
+                return ElevatedButton(
+                  onPressed: () {
+                    context.read<LogoutBloc>().add(const LogoutEvent.logout());
+                  },
+                  child: const Text('Logout'),
+                );
+              },
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          },
+        ),
       ),
     ];
   }
@@ -50,7 +86,7 @@ class _HomePageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(token)),
+      // appBar: AppBar(title: Text(token)),
       key: _scaffoldKey,
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Theme.of(context).primaryColor,
